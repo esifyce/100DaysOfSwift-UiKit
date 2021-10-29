5de61aefc934529a0a8c01b51a3b8377b8cce80e
primarydose <49156359+primarydose@users.noreply.github.com>
2021-10-29 19:14:26 +0600

    Add files via upload

---
+++ 31.Day-projectB-AdvancedAutoLayout/31.Day-projectB-AdvancedAutoLayout/ViewController.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  30.Day-projectB-AdvancedAutoLayout
+//
+//  Created by Sabir Myrzaev on 29.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        // MARK: - Constraints
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
+            
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+                
+            }
+            previous = label
+            
+        }
+
+    }
+}
+
