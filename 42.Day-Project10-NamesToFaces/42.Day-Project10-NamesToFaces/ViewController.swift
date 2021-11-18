e6c0d50183d38df6b903e2809d1db56222cc2c1f
primarydose <49156359+primarydose@users.noreply.github.com>
2021-11-19 00:03:47 +0600

    Add files via upload

---
+++ 42.Day-Project10-NamesToFaces/42.Day-Project10-NamesToFaces/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  42.Day-Project10-NamesToFaces
+//
+//  Created by Sabir Myrzaev on 17.11.2021.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else { fatalError(" ERROR: deqreuse cell") }
+        return cell
+    }
+    
+
+}
+
