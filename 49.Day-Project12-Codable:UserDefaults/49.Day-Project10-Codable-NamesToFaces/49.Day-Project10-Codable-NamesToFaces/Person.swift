d85d3fa5fb016e6499e5d6754615bbe2c9f38011
primarydose <49156359+primarydose@users.noreply.github.com>
2021-11-23 22:55:47 +0600

    Add files via upload

---
+++ 49.Day-Project12-Codable:UserDefaults/49.Day-Project10-Codable-NamesToFaces/49.Day-Project10-Codable-NamesToFaces/Person.swift
@@ -0,0 +1,19 @@
+//
+//  Person.swift
+//  42.Day-Project10-NamesToFaces
+//
+//  Created by Sabir Myrzaev on 19.11.2021.
+//
+
+import UIKit
+
+class Person: NSObject, Codable {
+
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+}
---
+++ 49.Day-Project12-Codable:UserDefaults/49.Day-Project10-Codable-NamesToFaces/49.Day-Project10-Codable-NamesToFaces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  42.Day-Project10-NamesToFaces
+//
+//  Created by Sabir Myrzaev on 18.11.2021.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+}
