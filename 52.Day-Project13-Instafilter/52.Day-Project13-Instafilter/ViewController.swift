ca154aa213884f8a781b98d1cb9102bfc821925e
primarydose <49156359+primarydose@users.noreply.github.com>
2021-11-29 14:25:28 +0600

    Add files via upload

---
+++ 52.Day-Project13-Instafilter/52.Day-Project13-Instafilter/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  52.Day-Project13-Instafilter
+//
+//  Created by Sabir Myrzaev on 29.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var intensity: UISlider!
+    var currentImage: UIImage!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = "Instafilter"
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
+
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        
+    }
+    
+    @objc func importPicture() {
+        let picker = UIImagePickerController()
+        picker.allowsEditing = true
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let image = info[.editedImage] as? UIImage else { return }
+        dismiss(animated: true)
+        currentImage = image
+    }
+    
+}
+
