462a157b056cf42b6b45da20155f00c0b8312234
primarydose <49156359+primarydose@users.noreply.github.com>
2022-01-02 22:19:36 +0600

    Add files via upload

---
+++ 64.Day-Project18-Debugging/64.Day-Project18-Debugging/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  64.Day-Project18-Debugging
+//
+//  Created by Sabir Myrzaev on 02.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+    }
+
+
+}
+
